3e76925f7e776ed3ee9867868e2b476162ef870a
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2022-11-02 19:23:16 +0900

    chore: add regression

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@chakra-ui/react";
 export const Page = () => {
   return (
     <article>
-      <Button backgroundColor={"emerald.500"} size={"lg"}>
+      <Button backgroundColor={"blackAlpha.700"} size={"lg"}>
         Button
       </Button>
     </article>
